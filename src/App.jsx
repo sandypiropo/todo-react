ea562e053405ae8e158a8271dbcf8d921f5c8044
sandypiropo <121200148+sandypiropo@users.noreply.github.com>
2023-09-27 03:14:05 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,53 @@
 import React, { useState } from "react"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("aaaa")
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+  }
+
+  console.log(todos)
 
   return (
     <div>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label>New Item</label>
-          <input value={newItem} type="text" id="item"/>
-        </div>  
+          <input
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text"
+            id="item" />
+        </div>
         <button className="btn">Add</button>
       </form>
-      <h1 className="header">Todo list</h1> 
-
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn-2">Delete</button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
-} 
+}
 
 
 
-    /*      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox">
-              Item 1
-            </input>
-            <button className="btn-2">Delete</button>
-          </label>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox">
-              Item 2
-            </input>
-            <button className="btn-2">Delete</button>
-          </label>
-        </li>
-      </ul>  */
+/* */
